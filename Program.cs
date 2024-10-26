0109814f8b1715137c9be19b87e554c43242fe07
StrawmanDave <davidgb.strawman@gmail.com>
2024-10-26 12:21:41 -0700

    Working Long game

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//David 10/26/24 The Long Game
+
+//Clears the screen for better viewing
+Console.Clear();
+int score = 0;
+
+Console.Write($"Please enter your name ");
+string username = Console.ReadLine();
+
+//Now how to check if the users file already exists access it and keep adding scores
+if(username == Path.GetFileNameWithoutExtension($"/home/david/david-programming-1400/The-Long-Game/{username}"))
+{
+   string oldScore = File.ReadAllText($"{username}.txt");
+   score = Convert.ToInt32(oldScore);
+}
+
+// Now how to make a score and add 1 to it for every keypressed
+Console.Write("Start masshing buttons on the keyboard!");
+while(true)
+{
+
+    if(Console.ReadKey().Key == ConsoleKey.Enter)
+    {
+        break;
+    }else
+    {
+        score = score + 1;
+    }
+    Console.WriteLine();
+    Console.Write($"You current score is now {score}. Now press any key to gain points. ");
+    
+};
+
+string saveScore = $"{Convert.ToString(score)}";
+
+
+// How to save score the file username?
+File.WriteAllText($"{username}.txt", saveScore);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # The-Long-Game
+This is for the challenge the long game out of the C# Player's Guide Fifth edition. This challenge is located on page 315 of the book.
